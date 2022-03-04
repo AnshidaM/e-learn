74cf66efd514fbf11bbe1de7565f8e82daad6d33
anshida <anshidasherinm7@gmail.com>
2022-03-04 15:42:28 +0530

    commit

--- ELearn/ELearnapp/models.py
+++ ELearn/ELearnapp/models.py
@@ -3,3 +3,15 @@ class verifylogin(models.Model):
     uname = models.CharField(max_length = 100)
     epass = models.CharField(max_length=100)
     ukey = models.CharField(max_length=1)
+    
+class login(models.Model):
+    uid = models.CharField(max_length = 100)
+    toflin = models.CharField(max_length=50)
+    toflout = models.CharField(max_length=50)
+    
+class admin(models.Model):
+    adid = models.CharField(max_length = 100)
+    adname = models.CharField(max_length=100)
+    adpswd = models.CharField(max_length=100)
+    adpno =  models.CharField(max_length=100)
+    
