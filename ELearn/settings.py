3b0f6e77d9083c4af4dc7b1d57af476ce642e46f
swalihmp <swalihmp438368@gmail.com>
2022-04-07 14:29:18 +0530

    settings

--- ELearn/settings.py
+++ ELearn/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'ELearnapp'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'ELearn.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -76,7 +77,7 @@ WSGI_APPLICATION = 'ELearn.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'elearn',
+        'NAME': 'elearndb',
         'USER' : 'elearn',
         'PASSWORD' : 'elearn',
         'HOST' : '193.161.193.99',
