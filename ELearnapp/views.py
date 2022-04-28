b4c468ba7cbad29d101d72f5b304bf9fa26d819b
swalihmp <swalihmp438368@gmail.com>
2022-04-28 14:02:28 +0530

    new

--- ELearnapp/views.py
+++ ELearnapp/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
-
+from django.shortcuts import redirect,render
+from django.contrib.auth import authenticate,login, logout
+from django.contrib import messages
+from ELearnapp.models import teachers
+from mysqlx import Result
 # Create your views here.
+
+def showsubsonaddworks(request):
+    results=teachers.objects.all
+    return render(request,"addworks.html",{"showsubs":results})
+
+def showdepsonaddworks(request):
+    results=teachers.objects.all
+    return render(request,"addworks.html",{"showdeps":results})
+
+
+def studregview(request):
+    return render(request,"studreg.html")
