4c3a141c2b182919f8b22215197fff6039d659e7
swalihmp <swalihmp438368@gmail.com>
2022-05-04 21:17:28 +0530

    04/05

--- ELearnapp/urls.py
+++ ELearnapp/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
-from .views import studregview
+from .views import homepageview
 
 urlpatterns = [
-    path('admin/',studregview,name='studentregistration')
+    path('',homepageview,name = 'homepage'),
 ]
--- ELearnapp/views.py
+++ ELearnapp/views.py
@@ -1,18 +1,8 @@
 from django.shortcuts import redirect,render
 from django.contrib.auth import authenticate,login, logout
 from django.contrib import messages
-from ELearnapp.models import teachers
 from mysqlx import Result
 # Create your views here.
 
-def showsubsonaddworks(request):
-    results=teachers.objects.all
-    return render(request,"addworks.html",{"showsubs":results})
-
-def showdepsonaddworks(request):
-    results=teachers.objects.all
-    return render(request,"addworks.html",{"showdeps":results})
-
-
-def studregview(request):
-    return render(request,"studreg.html")
+def homepageview(request):
+    return render(request,"ELearnapp/login.html")
